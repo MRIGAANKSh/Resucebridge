84939226258f5d817da90f12624201a6d113cd09
Priyanshug254 <guptapriyanshu254@gmail.com>
2024-05-25 16:00:25 +0530

    Version2

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ hamburger.onclick=function(){
     navbar=document.querySelector(".navbar");
     navbar.classList.toggle("active")
 }
+document.addEventListener('DOMContentLoaded', () => {
+    const faqs = document.querySelectorAll('.faq');
 
-
-
-    
+    faqs.forEach(faq => {
+        faq.addEventListener('click', () => {
+            faq.classList.toggle('active');
+            const toggle = faq.querySelector('.faq-toggle');
+            toggle.textContent = faq.classList.contains('active') ? '−' : '+';
+        });
+    });
+});
